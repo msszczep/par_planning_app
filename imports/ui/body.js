2b10825fbb30c75e1e3d22c456ffce9da6c7f6a3
Mitchell Szczepanczyk <mitchell@szcz.org>
2021-02-15 20:34:43 -0600

    Updated with neighborhoods, workplaces

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,22 @@
 import { Template } from 'meteor/templating';
 import { Games } from '../api/games.js';
 import { CurrentState } from '../api/currentstate.js';
+import { Neighborhoods } from '../api/neighborhoods.js';
+import { Workplaces } from '../api/workplaces.js';
 
 import './body.html';
 
-Template.gamesList.helpers({
+Template.itemsList.helpers({
   gamesToShow() {
     return Games.find({});
+  },
+
+  neighborhoodsToShow() {
+    return Neighborhoods.find({});
+  },
+
+  workplacesToShow() {
+    return Workplaces.find({});
   },
 
   currentState() {
@@ -21,11 +31,39 @@ Template.body.events({
     Games.insert({createdBy: Accounts.user()._id, createdAt: Date()});
   },
 
+  'click .addworkplace' (event) {
+    event.preventDefault();
+
+    Workplaces.insert({createdBy: Accounts.user()._id, createdAt: Date()});
+  },
+
+  'click .addneighborhood' (event) {
+    event.preventDefault();
+
+    Neighborhoods.insert({createdBy: Accounts.user()._id, createdAt: Date()});
+  },
+
   'click .joingame' (event) {
     event.preventDefault();
     c = CurrentState.find({}).fetch({})[0];
     const target = event.target;
 
     CurrentState.update(c._id, {$set: {currentGame: target.value},});
+  }, 
+
+  'click .joinneighborhood' (event) {
+    event.preventDefault();
+    c = CurrentState.find({}).fetch({})[0];
+    const target = event.target;
+
+    CurrentState.update(c._id, {$set: {currentNeighborhood: target.value},});
+  },
+
+  'click .joinworkplace' (event) {
+    event.preventDefault();
+    c = CurrentState.find({}).fetch({})[0];
+    const target = event.target;
+
+    CurrentState.update(c._id, {$set: {currentWorkplace: target.value},});
   }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import '../imports/api/games.js'
 import '../imports/api/currentstate.js'
+import '../imports/api/neighborhoods.js'
+import '../imports/api/workplaces.js'
 import { Meteor } from 'meteor/meteor';
 import { CurrentState } from '../imports/api/currentstate.js';
 
