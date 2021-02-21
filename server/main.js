347be479747c4bd8044023df8b160cccb5bf3376
Mitchell Szczepanczyk <mitchell@szcz.org>
2021-02-20 22:56:00 -0600

    Revisions: Hard-code workplaces, neighborhoods, with names.

--- server/main.js
+++ server/main.js
@@ -4,9 +4,22 @@ import '../imports/api/neighborhoods.js'
 import '../imports/api/workplaces.js'
 import { Meteor } from 'meteor/meteor';
 import { CurrentState } from '../imports/api/currentstate.js';
+import { Games } from '../imports/api/games.js';
+import { Workplaces } from '../imports/api/workplaces.js';
+import { Neighborhoods } from '../imports/api/neighborhoods.js';
 
-if (CurrentState.find().count() === 0) {
-  CurrentState.insert({currentGame: 1});
+if (Games.find().count() === 0) {
+  Games.insert({createdAt: Date()});
+}
+ 
+if (Workplaces.find().count() === 0) {
+  Workplaces.insert({createdAt: Date(), name: "Bakery"});
+  Workplaces.insert({createdAt: Date(), name: "Brewery"});
+  Workplaces.insert({createdAt: Date(), name: "Pizzeria"});
 }
 
-
+if (Neighborhoods.find().count() === 0) {
+  Neighborhoods.insert({createdAt: Date(), name: "Goldman Green"});
+  Neighborhoods.insert({createdAt: Date(), name: "Rocker Hill"});
+  Neighborhoods.insert({createdAt: Date(), name: "Bakunin Bay"});
+}
