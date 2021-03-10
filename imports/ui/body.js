5bb58f7cc68560afab3b3cff0aaa858a14dc2764
Mitchell Szczepanczyk <mitchell@szcz.org>
2021-03-09 21:08:52 -0600

    got price adjustment rule updated

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -411,9 +411,114 @@ Template.resetpane.events({
 
   'click .newiteration' (event) {
     event.preventDefault();
-    const i = IterationCounter.find({}).fetch({})[0];
-    const newincrement = i.iteration + 1;
-    IterationCounter.update(i._id, {$set: {iteration: newincrement}});
+    ic = IterationCounter.find({}).fetch({})[0];
+    c = ic.iteration;
+
+    newpricefunction = function (supply, demand, price) {
+      surplus = supply - demand;
+      v = (Math.abs(2 * surplus) / (demand + supply));
+      w = v * (1.05 - Math.pow(0.5, v));
+      if (surplus > 0) {
+        newprice = (1 - w) * price * 1000 / 1000;
+      } else if (surplus < 0) {
+        newprice = (1 + w) * price * 1000 / 1000;
+      } else {
+        newprice = price * 1000 / 1000;
+      }
+      return newprice;
+    };
+
+    iData = IterationData.find({iteration: c}).fetch({});
+    totalpizzademand = iData.reduce(function (accumulator, iterationdatum) {
+      return accumulator + Number(iterationdatum.pizza);
+    }, 0);
+    totalbeerdemand = iData.reduce(function (accumulator, iterationdatum) {
+      return accumulator + Number(iterationdatum.beer);
+    }, 0);
+    totalbreaddemand = iData.reduce(function (accumulator, iterationdatum) {
+      return accumulator + Number(iterationdatum.bread);
+    }, 0);
+
+    const laborConvert = {"bread": {"A": 1.25, "B": 1, "C": 0.75, "": 1},
+                          "beer": {"A": 1.75, "B": 1, "C": 0.25, "": 1},
+                          "pizza": {"A": 1.5, "B": 1, "C": 0.5, "": 1}};
+    const wheatConvert = {"bread": {"A": 1.25, "B": 1, "C": 0.75, "": 1},
+                          "beer": {"A": 1.75, "B": 1, "C": 0.25, "": 1},
+                          "pizza": {"A": 1.5, "B": 1, "C": 0.5, "": 1}};
+    a = Actors.find({}).fetch({}).length;
+    frequencyfunction = function (acc, iterationdatum) {
+      if (acc[iterationdatum.recipe] === undefined) {
+        acc[iterationdatum.recipe] = 1;
+      } else {
+        acc[iterationdatum.recipe] += 1;
+      }
+      return acc;
+    };
+    sortingfunction = function (a, b) { return a[1] + b[1] };
+    pctdiff = function (a, b) { return Math.round(Math.abs(a - b) / ((a + b) / 2) * 100 * 1000) / 1000 }
+
+    pizzadata = IterationData.find({iteration: c, workplace: "Pizzeria"}).fetch({});
+    beerdata = IterationData.find({iteration: c, workplace: "Brewery"}).fetch({});
+    breaddata = IterationData.find({iteration: c, workplace: "Bakery"}).fetch({});
+
+    pizzahours = pizzadata.reduce(function (accumulator, iterationdatum) {
+      return accumulator + Number(iterationdatum.hoursToWork);
+    }, 0);
+    breadhours = breaddata.reduce(function (accumulator, iterationdatum) {
+      return accumulator + Number(iterationdatum.hoursToWork);
+    }, 0);
+    beerhours = beerdata.reduce(function (accumulator, iterationdatum) {
+      return accumulator + Number(iterationdatum.hoursToWork);
+    }, 0);
+
+    pizzarecipes = pizzadata.reduce(frequencyfunction, {});
+    breadrecipes = breaddata.reduce(frequencyfunction, {});
+    beerrecipes = beerdata.reduce(frequencyfunction, {});
+
+    pizzasortable = [];
+    beersortable = [];
+    breadsortable = [];
+
+    for (i in pizzarecipes) {
+      pizzasortable.push([i, pizzarecipes[i]])
+    };
+    for (i in breadrecipes) {
+      breadsortable.push([i, breadrecipes[i]])
+    };
+    for (i in beerrecipes) {
+      beersortable.push([i, beerrecipes[i]])
+    };
+
+    breadsortable.sort(sortingfunction);
+    beersortable.sort(sortingfunction);
+    pizzasortable.sort(sortingfunction);
+
+    winningpizzarecipe = pizzasortable.length === 0 ? '' : pizzasortable[0][0];
+    winningbreadrecipe = breadsortable.length === 0 ? '' : breadsortable[0][0];
+    winningbeerrecipe = beersortable.length === 0 ? '' : beersortable[0][0];
+
+    breadsupplyr = Math.round(breadhours * 1000 / laborConvert["bread"][winningbreadrecipe]) / 1000;
+    pizzasupplyr = Math.round(pizzahours * 1000 / laborConvert["pizza"][winningpizzarecipe]) / 1000;
+    beersupplyr = Math.round(beerhours * 1000 / laborConvert["beer"][winningbeerrecipe]) / 1000;
+    totallaborsupply = a * 10;
+    totalwheatsupply = a * 12;
+
+    pizzawheatdemand = (pizzasupplyr * wheatConvert["pizza"][winningpizzarecipe]);
+    beerwheatdemand = (beersupplyr * wheatConvert["beer"][winningbeerrecipe]);
+    breadwheatdemand = (breadsupplyr * wheatConvert["bread"][winningbreadrecipe]);
+    wheatdemandr = pizzawheatdemand + beerwheatdemand + breadwheatdemand;
+
+    p = Prices.find({iteration: c}).fetch({})[0];
+
+    newpizzaprice = newpricefunction(pizzasupplyr, totalpizzademand, p.pizza);
+    newbreadprice = newpricefunction(breadsupplyr, totalbreaddemand, p.bread);
+    newbeerprice = newpricefunction(beersupplyr, totalbeerdemand, p.beer);
+    newwheatprice = newpricefunction(totalwheatsupply, wheatdemandr, p.wheat);
+
+    const newincrement = c + 1;
+    IterationCounter.update(ic._id, {$set: {iteration: newincrement}});
+
+    Prices.insert({iteration: newincrement, pizza: newpizzaprice, beer: newbeerprice, bread: newbreadprice, wheat: newwheatprice, labor: p.labor});
   }
 
 });
